393157b57015cb1f28531a7325175da35e7efe49
Mikey-wqq <sidneymichael@gmail.com>
2024-12-11 12:14:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,85 @@
+var developer = 'banjo'
+var designer = 'Sidney'
+var accountant = 'Damola'
+
+// concatenation
+var sentence = ' Banjo needs ' + designer + ' to help with designs for his client ' + accountant + ' website '
+console.log(sentence);
+console.log(sentence.length);
+console.log(sentence.toUpperCase());
+console.log(sentence.toLowerCase());
+
+var age = 17
+if(age >= 18 ){
+console.log('you can vote');
+}else{
+    console.log('you are too young to vote');
+    
+}
+
+var age = 25
+if(age >= 18 ){
+console.log('you can vote');
+}else{
+    console.log('you are too young to vote');
+    
+}
+
+var student = false
+if(student == true){
+    console.log('free ticket');
+    
+}else{
+    console.log('pay your complete money');
+    
+}
+
+var student = true
+if(student == true){
+    console.log('free ticket');
+    
+}else{
+    console.log('pay your complete money');
+    
+}
+
+var me = 'tired'
+if(me != 'tired'){
+    console.log('continue working');
+    
+}else{
+    console.log('get a coffee');
+    
+}
+
+var presido = 'tpain'
+if(presido != 'tpain'){
+    console.log('life is good');
+    
+}else{
+    console.log('sapa dey for everybody');
+    
+}
+
+var job = 'developer'
+if(job == 'lawyer'){
+    console.log('your bill is 200k');
+    
+}else if(job == 'police'){
+    console.log('your bill is 1 million');
+    
+}else if(job == 'influencer'){
+    console.log('your bill is 600k');
+    
+}else if(job == 'instructor'){
+    console.log('your bill is 50k');
+    
+}else{
+    console.log('your bill is 100k');
+    
+}
+
+
+
+
+
